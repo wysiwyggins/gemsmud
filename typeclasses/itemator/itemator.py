57570e15383bd8c67908fc6de1a8cc40b4085e03
Wiley Wiggins <weevil@wileywiggins.com>
2020-02-27 20:30:00 -0800

    Update itemator.py

--- typeclasses/itemator/itemator.py
+++ typeclasses/itemator/itemator.py
@@ -67,7 +67,6 @@ class Item(DefaultObject):
             "key": self.item_name,
             "typeclass": evennia.contrib.clothing.Clothing,
             "desc": self.item_description,
-            "location": none
         }
         return self.item_proto
 
@@ -90,7 +89,6 @@ class Item(DefaultObject):
             "key": self.item_name,
             "typeclass": "typeclasses.objects.Object",
             "desc": self.item_description,
-            "location": none
         }
         return self.item_proto
 
