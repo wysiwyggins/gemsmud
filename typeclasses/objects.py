375ed539231263131b3b777ef9228fc79099d720
Wiley Wiggins <weevil@wileywiggins.com>
2020-03-05 16:36:28 -0800

    Update objects.py

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -305,12 +305,16 @@ class Incinerator(DefaultObject):
 
         """
         super().at_object_receive(self, moved_obj, source_location, **kwargs)
-        self.caller.msg("Object received")
+        message = "Object received"
+        source_location.msg_contents("message")
         if moved_obj.db.typeclass == "typeclasses.characters.Character":
-            self.caller.msg("{objectname} is making a very embarrassing racket about being in the incinerator.".format(
-                objectname=moved_obj.name))
+            message = "the {objectname} is making a very embarassing racket about being on fire.".format(
+                objectname=moved_obj.name)
+            source_location.msg_contents(message)
         else:
-            self.caller.msg("the {objectname} bursts into flames inside the incinerator".format(objectname=moved_obj.name))
+            message = "the {objectname} bursts into flames inside the incinerator.".format(
+                objectname=moved_obj.name)
+            source_location.msg_contents(message)
         
         self.execute_cmd("destroy" + moved_obj)
 
