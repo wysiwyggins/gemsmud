599ebe8819c8d65d7405d4d6f9651d1222565d30
Wiley Wiggins <weevil@wileywiggins.com>
2020-03-05 17:32:56 -0800

    Update objects.py

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -305,7 +305,7 @@ class Incinerator(DefaultObject):
 
         """
         self.location.msg_contents(message)
-        if moved_obj.is_typeclass("typeclasses.characters.Character"):
+        moved_obj.is_typeclass("typeclasses.characters.Character"):
             message = "the {objectname} is making a very embarassing racket about being on fire.".format(objectname=moved_obj.name)
             self.location.msg_contents(message)
         else:
