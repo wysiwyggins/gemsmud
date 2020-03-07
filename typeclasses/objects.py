b752224bfc8d8c8f6b216cde040af84e4152a9da
Wiley Wiggins <weevil@wileywiggins.com>
2020-03-07 00:56:56 -0800

    Update objects.py

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -340,7 +340,6 @@ class Counter(Readable):
         if cnt > 100:
             overcount = 100 - cnt
             warningtext = "|500Warning, Zone 25 is now {overcount} item(s) over allowed limits.|n".format(overcount=overcount)
-        self.db.readable_text = signtext + "\n" + countertext + "\n" + warningtext
-        
+        self.db.readable_text = signtext + "\n" + countertext + "\n" + warningtext + breakdowntext
         self.db.desc = signtext + "\n" + countertext + "\n" + warningtext + breakdowntext
         super().at_desc(looker)
