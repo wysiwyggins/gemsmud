32fea4779b142a627adcaeb308ec329cc86b4327
Wiley Wiggins <weevil@wileywiggins.com>
2020-03-17 14:49:50 -0500

    Update objects.py

--- typeclasses/objects.py
+++ typeclasses/objects.py
@@ -330,7 +330,7 @@ class Counter(Readable):
         for item in cursed:
             cursedList.append(item.name)
         cursedCount = len(cursedList)
-        cursedNames = "\n".join(cursedList)
+        cursedNames = "\n\t".join(cursedList)
         cnt_omit = Object.objects.filter(db_typeclass_path="typeclasses.rooms.DefaultRoom, typeclasses.hybrid_room.HybridRoom, typeclasses.exits.Exit").count()
         cnt_all = ObjectDB.objects.all().count()
         cnt = cnt_all - cnt_omit
@@ -346,8 +346,7 @@ class Counter(Readable):
         if cnt > 1000:
             overcount = 1000 - cnt
             warningtext = "|500Warning, Zone 25 is now {overcount} item(s) over allowed limits.|n".format(overcount=overcount)
-        self.db.readable_text = signtext + "\n" + countertext + "\n" + warningtext + breakdowntext + \
-            "\n|500Cursed Objects: {cursedCount}".format(
+        self.db.readable_text = signtext + "\n" + countertext + "\n" + warningtext + breakdowntext + "\n|500Cursed Objects: {cursedCount}".format(
                 cursedCount=cursedCount) + "\n" + cursedNames + "|n"
         self.db.desc = signtext + "\n" + countertext + "\n" + warningtext + breakdowntext +"\n" + "Check your own inventory at any time with |555inv|n."
         super().at_desc(looker)
